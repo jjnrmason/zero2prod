34e908c1cbdc19819bcc536e2fc37a52e96792fb
John Mason <johnjnrmason@gmail.com>
2025-03-01 13:32:57 +0000

    Add API

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+use zero2prod::{configuration::get_configuration, startup::run};
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let configurtion = get_configuration().expect("Failed to read configuration");
+    let address = format!("127.0.0.1:{}", configurtion.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener)?.await
 }
