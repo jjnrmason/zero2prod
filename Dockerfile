f67dd1deebef2413e6a303796c73be84c34f53cd
John Mason <johnjnrmason@gmail.com>
2025-03-13 21:51:46 +0000

    Add docker file

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,28 @@
-FROM rust:1.63.0
-
+FROM lukemathwalker/cargo-chef:latest-rust-1.85.0 as chef
 WORKDIR /app
-
 RUN apt update && apt install lld clang -y
-
+FROM chef as planner
 COPY . .
+# Compute a lock-like file for our project
+RUN cargo chef prepare --recipe-path recipe.json
+FROM chef AS builder
+COPY --from=planner /app/recipe.json recipe.json
+# Build our project dependencies, not our application!
+RUN cargo chef cook --release --recipe-path recipe.json
+# Up to this point, if our dependency tree stays the same,
+# all layers should be cached.
+COPY . .
+ENV SQLX_OFFLINE=true
+# Build our project
+RUN cargo build --release --bin zero2prod
 
-RUN cargo build --release
-
-ENTRYPOINT [ "./target/release/zero2prod" ]
+# Runtime stage
+FROM rust:1.85.0-slim AS runtime
+WORKDIR /app
+# Copy the compiled binary from the builder environment
+# to our runtime environment
+COPY --from=builder /app/target/release/zero2prod zero2prod
+# We need the configuration file at runtime!
+COPY configuration configuration
+ENV APP_ENVIRONMENT=production
+ENTRYPOINT ["./zero2prod"]
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use secrecy::ExposeSecret;
 use std::net::TcpListener;
 
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use zero2prod::{
     configuration::get_configuration,
     startup::run,
@@ -10,15 +10,19 @@ use zero2prod::{
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
+    let subscriber = get_subscriber(
+        "zero2prod".into(), "info".into(), std::io::stdout
+    );
     init_subscriber(subscriber);
-
-    let configurtion = get_configuration().expect("Failed to read configuration");
-    let connection_pool =
-        PgPool::connect(&configurtion.database.connection_string().expose_secret())
-            .await
-            .expect("Failed to connect to Postgres.");
-    let address = format!("127.0.0.1:{}", configurtion.application_port);
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(
+            &configuration.database.connection_string().expose_secret()
+        )
+        .expect("Failed to create Postgres connection pool.");
+    let address = format!("{}:{}", configuration.application.host,  configuration.application.port);
     let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool)?.await
+    run(listener, connection_pool)?.await?;
+    Ok(())
 }
