2ef15a0fb48cfee983be8841f6300e318a505a0b
John Mason <johnjnrmason@gmail.com>
2025-03-15 10:46:52 +0000

    Formatting and logging

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use secrecy::ExposeSecret;
 use std::net::TcpListener;
 
 use sqlx::postgres::PgPoolOptions;
@@ -10,18 +9,16 @@ use zero2prod::{
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let subscriber = get_subscriber(
-        "zero2prod".into(), "info".into(), std::io::stdout
-    );
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPoolOptions::new()
         .acquire_timeout(std::time::Duration::from_secs(2))
-        .connect_lazy(
-            &configuration.database.connection_string().expose_secret()
-        )
-        .expect("Failed to create Postgres connection pool.");
-    let address = format!("{}:{}", configuration.application.host,  configuration.application.port);
+        .connect_lazy_with(configuration.database.with_db());
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await?;
     Ok(())
